1f773d3fefe4491894acab71b766e8f3ba21d6a5
Peperysnow <156832627+Peperysnow@users.noreply.github.com>
2025-04-02 11:16:07 +0300

    02.04.2025

---
+++ II. Semestris/JavaScript/02.04.2025/kalkulators.js
@@ -0,0 +1,35 @@
+function myfunction()
+{
+ let 1 = document.getElementById("1");
+ console.log(1);
+ let 2 = document.getElementById("2");
+ console.log(2);
+ let 3 = document.getElementById("3");
+ console.log(3);
+ let 4 = document.getElementById("4");
+ console.log(4);
+ let 5 = document.getElementById("5");
+ console.log(5);
+ let 6 = document.getElementById("6");
+ console.log(6);
+ let 7 = document.getElementById("7");
+ console.log(7);
+ let 8 = document.getElementById("8");
+ console.log(8);
+ let 9 = document.getElementById("9");
+ console.log(9);
+
+ if{
+
+ }
+
+ 
+
+
+
+
+
+
+
+
+}
--- II. Semestris/JavaScript/02.04.2025/script.js
+++ II. Semestris/JavaScript/02.04.2025/script.js
@@ -5,19 +5,64 @@ function myFunction() // myFunction - nosaukums
  console.log(h2_element);
  let name_element = document.getElementById("Name_input")
  h2_element.innerText = "Hello, " + name_element.value + " !"; // += pieliek vel vārdus.
-
+ //     email_element.value.match(/[!@]/)) ari strādā
 }
 */
-function myFunction() // myFunction - nosaukums
+function myFunction()
 {
  let h2_element = document.getElementById("h2_id")
  console.log(h2_element);
  let name_element = document.getElementById("name_input");
  let surname_element = document.getElementById("surname_input");
  let age_element = document.getElementById("age_input");
+ let email_element = document.getElementById("email_input");
+ let table_element = document.getElementById("myTable")
+ 
+ if(name_element.value =="" ||   
+   !name_element.value.match(/[A-z]/) || 
+   name_element.value.match(/[0-9]/))
+{
+   console.error("Name contains invalid symbol!")
+   return; 
+}
+
+if(surname_element.value =="" ||   
+   !surname_element.value.match(/[A-z]/) || 
+   surname_element.value.match(/[0-9]/))
+{
+   console.error("Surname contains invalid symbol!")
+   return; 
+}
+
+if(age_element.value =="" ||   
+   age_element.value.match(/[A-z]/) || 
+   !age_element.value.match(/[0-9]/))
+{
+   console.error("Age contains invalid symbol!")
+   return; 
+}
+
+if(email_element.value =="" ||
+   !email_element.value.match(/[@]/))
+
+{
+   console.error("Email does not contain @ symbol!")
+   return; 
+}
+
 h2_element.innerText = "Hello, " +
-name_element.value + " " + surname_element.value + " "
- + age_element.value;
+name_element.value + " " + surname_element.value + " " + age_element.value + " " + email_element.value;
+
+let row = table_element.insertRow();
+let cell1 = row.insertCell();
+let cell2 = row.insertCell();
+let cell3 = row.insertCell();
+
+cell1.innerHTML = name_element.value;
+cell2.innerHTML = surname_element.value;
+cell3.innerHTML = email_element.value;
+
+
 }
 
 
