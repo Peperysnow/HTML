69191b25c8a2569f91b116c3fa9fd77c4eb84922
Peperysnow <156832627+Peperysnow@users.noreply.github.com>
2025-04-23 11:13:48 +0300

    Yes

--- II. Semestris/23.04.2025/script.js
+++ II. Semestris/23.04.2025/script.js
@@ -1,17 +1,54 @@
 let click_Power = 1;
+let upgrade_count = 1;
+let upgrade2_count = 0;
 
 
-function click_Handler ()
+function click_handler ()
 {
 
-    let user_Points = document.getElementById("points").innerText;
+    let user_Points = Number(document.getElementById("points").innerText);
     console.log(user_Points);
     user_Points = user_Points + click_Power;
     document.getElementById("points").innerText = user_Points;
+
 }
 
-function upgrade_Handler() {
+function upgrade_handler() {
 
     let user_Points = document.getElementById("points").innerText;
+    let upgrade_Price = 10 * upgrade_count;
+    if(user_Points >= upgrade_Price)
+{
+ click_Power++;
+ document.getElementById("points").innerText = user_Points - 15
+ document.getElementById("Upgrade1").value = upgrade_Price;
+}
+else
+{
+  console.warn("Points < 15");
+}
+}
+function upgrade2_handler()
+{
+let user_Points = document.getElementById("points").innerText;
+if(user_Points >= 30)     {
+
+    const myInterval = setInterval(passive_income, 1000);
+}
+    {
+upgrade2_count++;
+document.getElementById("points").innerText = user_Points - 30;
 
 }
+}
+function passive_income()
+{
+  let points = document.getElementById("points").innerText;
+  points += 1 * upgrade2_count;
+  document.getElementById("points").innerText = points;
+}
+if(upgrade2_count > 0)
+ {
+    
+ }
+ 
